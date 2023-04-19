faf94fb3969c48f42dafc9f46613ac37d0fe13d2
Scott Rosenberg <48493016+vrabbi@users.noreply.github.com>
2023-04-19 16:09:59 +0300

    Delete scripts directory

--- scripts/setup-keyvault-env-variables-template.sh
+++
@@ -1,1 +0,0 @@
-export KEY_VAULT=acme-fitness-kv-CHANGE-ME     # Unique name for Azure Key Vault. Replace CHANGE_ME with the 4 unique characters that were created as part of ARM template in Section 3.
